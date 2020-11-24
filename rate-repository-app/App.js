275b98975933158ff52e6610f49bc26ed11e2cdb
Lin Htet Swe <linhtetswe2012@gmail.com>
2020-11-24 20:06:56 +0630

    added eslint

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -1,6 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
@@ -14,8 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#df1234',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#df1234",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
